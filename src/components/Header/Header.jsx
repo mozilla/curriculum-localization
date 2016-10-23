17224939821767d21dc3d44f6f0a6198a276b7e2
Gideon Thomas <gideon@mozillafoundation.org>
2016-10-23 14:04:24 -0400

    More content

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import Strings from "../../strings.json";
-
 class Header extends React.Component {
   render() {
     return (
--- src/pages/Instructions/Instructions.jsx
+++ src/pages/Instructions/Instructions.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 
+import Strings from "../../strings.json";
+
 class Instructions extends React.Component {
   render() {
+    const text = Strings.INSTRUCTIONS;
+
     return (
       <div className="container">
         <div className="row">
-          <div className="col-xs-12 col-lg-10 col-lg-push-2">
-            <h3>How to Contribute</h3>
+          <div className="col-xs-12 col-lg-10 push-lg-1">
+            <div className="row">
+
+              {/* Main CTA */}
+              <div id="main-cta" className="col-xs-12 col-md-12 col-lg-10 push-lg-1">
+                <h4>{ text.CTA.TITLE }</h4>
+                <p>{
+                  text.CTA.CONTENT
+                  .replace("{ CURRICULUM_MODULE_NAME }", Strings.CURRICULUM_MODULE_NAME)
+                }</p>
+              </div>
+
+              {/* How to contribute */}
+              <div id="campaign-info" className="col-xs-12 col-md-12 col-lg-10">
+                <h4>{ text.CAMPAIGN_INFO.TITLE }</h4>
+                <p>{ text.CAMPAIGN_INFO.CONTENT }</p>
+              </div>
+
+            </div>
+
           </div>
         </div>
       </div>
