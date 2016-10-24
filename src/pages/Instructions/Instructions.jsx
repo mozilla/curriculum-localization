b6226595ec6409afe87a2aa1a7779939a8c9d688
Gideon Thomas <gideon@mozillafoundation.org>
2016-10-24 10:57:05 -0400

    About the Campaign

--- src/pages/Instructions/Instructions.jsx
+++ src/pages/Instructions/Instructions.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 import Strings from "../../strings.json";
 
+function html(htmlContent) {
+  return { __html: htmlContent };
+}
+
 class Instructions extends React.Component {
   render() {
     const text = Strings.INSTRUCTIONS;
@@ -14,17 +18,35 @@ class Instructions extends React.Component {
 
               {/* Main CTA */}
               <div id="main-cta" className="col-xs-12 col-md-12 col-lg-10 push-lg-1">
-                <h4>{ text.CTA.TITLE }</h4>
-                <p>{
-                  text.CTA.CONTENT
-                  .replace("{ CURRICULUM_MODULE_NAME }", Strings.CURRICULUM_MODULE_NAME)
-                }</p>
+                <h4 dangerouslySetInnerHTML={ html(text.CTA.TITLE) }></h4>
+                <p dangerouslySetInnerHTML={
+                  html(
+                    text.CTA.CONTENT
+                    .replace("{ CURRICULUM_MODULE_NAME }", Strings.CURRICULUM_MODULE_NAME)
+                  )
+                }></p>
               </div>
 
-              {/* How to contribute */}
+              {/* About the campaign */}
               <div id="campaign-info" className="col-xs-12 col-md-12 col-lg-10">
-                <h4>{ text.CAMPAIGN_INFO.TITLE }</h4>
-                <p>{ text.CAMPAIGN_INFO.CONTENT }</p>
+                <h4 dangerouslySetInnerHTML={ html(text.CAMPAIGN_INFO.TITLE) }></h4>
+                <p dangerouslySetInnerHTML={
+                  html(
+                    text.CAMPAIGN_INFO.CONTENT_1
+                    .replace("{ CURRICULUM_MODULE_NAME }", Strings.CURRICULUM_MODULE_NAME)
+                  )
+                }></p>
+                <p dangerouslySetInnerHTML={ html(text.CAMPAIGN_INFO.CONTENT_2) }></p>
+              </div>
+
+            {/* How to Contribute */}
+              <div id="how-to-contribute" className="col-xs-12 col-md-12 col-lg-10">
+                <h4 dangerouslySetInnerHTML={ html(text.CONTRIBUTE.TITLE) }></h4>
+                <p dangerouslySetInnerHTML={
+                  html(
+                    text.CONTRIBUTE.CONTENT
+                  )
+                }></p>
               </div>
 
             </div>
